4f7052a7c5ec495146211e4aa749d5e4516f8bc9
Alejandro <alecarvalho950@gmail.com>
2023-11-15 00:03:22 -0300

    Criando variáveis para manipular o DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+var valorDigitado = window.document.getElementById('inputValor');
+var botaoEuro = window.document.getElementById('botaoEuro');
+var boatoDolar = window.document.getElementById('botaoDolar');
+var botaoLibra = window.document.getElementById('botaoLibra');
+var textoResposta = windo.document.getElementById('textoResposta');
